fa3c40ed01bfe8a57f6ca0ffe264edada3a7a3b7
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-08-12 02:34:31 +0600

    added full complete

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
   
+// document.addEventListener("DOMContentLoaded", function() {
+//     const accordionHeaders = document.querySelectorAll(".accordion-header");
+  
+//     accordionHeaders.forEach(header => {
+//       header.addEventListener("click", function() {
+//         // Toggle active class for the clicked accordion item
+//         this.parentElement.classList.toggle("active");
+        
+//         // Close other accordion items
+//         const activeItem = this.parentElement;
+//         const accordionItems = document.querySelectorAll(".accordion-item");
+//         accordionItems.forEach(item => {
+//           if (item !== activeItem) {
+//             item.classList.remove("active");
+//           }
+//         });
+//       });
+//     });
+//   });
+  
+
 document.addEventListener("DOMContentLoaded", function() {
     const accordionHeaders = document.querySelectorAll(".accordion-header");
-  
+
     accordionHeaders.forEach(header => {
-      header.addEventListener("click", function() {
-        // Toggle active class for the clicked accordion item
-        this.parentElement.classList.toggle("active");
-        
-        // Close other accordion items
-        const activeItem = this.parentElement;
-        const accordionItems = document.querySelectorAll(".accordion-item");
-        accordionItems.forEach(item => {
-          if (item !== activeItem) {
-            item.classList.remove("active");
-          }
+        header.addEventListener("click", function() {
+            // Toggle active class for the clicked accordion item
+            this.parentElement.classList.toggle("active");
+
+            // Toggle arrow icons
+            const arrowIcons = this.querySelectorAll(".arrow i");
+            arrowIcons.forEach(icon => {
+                icon.classList.toggle("fa-caret-up");
+                icon.classList.toggle("fa-caret-down");
+            });
+
+            // Close other accordion items
+            const activeItem = this.parentElement;
+            const accordionItems = document.querySelectorAll(".accordion-item");
+            accordionItems.forEach(item => {
+                if (item !== activeItem) {
+                    item.classList.remove("active");
+                    const icons = item.querySelectorAll(".arrow i");
+                    icons.forEach(icon => {
+                        icon.classList.add("fa-caret-down");
+                        icon.classList.remove("fa-caret-up");
+                    });
+                }
+            });
         });
-      });
     });
-  });
+});
   
