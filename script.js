94895114716a4c57c5b0d79c6437fb94627763dd
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-08-12 02:58:21 +0600

    added

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
   
-// document.addEventListener("DOMContentLoaded", function() {
-//     const accordionHeaders = document.querySelectorAll(".accordion-header");
-  
-//     accordionHeaders.forEach(header => {
-//       header.addEventListener("click", function() {
-//         // Toggle active class for the clicked accordion item
-//         this.parentElement.classList.toggle("active");
-        
-//         // Close other accordion items
-//         const activeItem = this.parentElement;
-//         const accordionItems = document.querySelectorAll(".accordion-item");
-//         accordionItems.forEach(item => {
-//           if (item !== activeItem) {
-//             item.classList.remove("active");
-//           }
-//         });
-//       });
-//     });
-//   });
-  
 
 document.addEventListener("DOMContentLoaded", function() {
     const accordionHeaders = document.querySelectorAll(".accordion-header");
